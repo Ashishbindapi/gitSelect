fb0b1c39fb054ac777edd63664f9c49033422f64
Ashishbindapi <ashish.k@bindapi.com>
2023-01-13 18:48:45 +0530

    c

--- api/districs.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include('./class/init.php');
-    $districController = new DistricController(new DistricModel($connection));
-    $data = $districController->districdata();
-    header("Content-type: application/json; charset=utf-8");
-    echo json_encode( $data );
